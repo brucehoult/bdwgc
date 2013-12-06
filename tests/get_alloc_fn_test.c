bd7558ade934d3fd8ad5731ceaca534e2ca1c1af
Bruce Hoult <bruce@hoult.org>
2013-12-06 15:10:45 +1300

    Added sample user implementation of GC_objdup to test program

--- tests/get_alloc_fn_test.c
+++ tests/get_alloc_fn_test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "gc.h"
 
 #define COUNT 1000
@@ -21,6 +22,31 @@ void test(char *p, int i, const char *name, void *want_fn){
   }
 }
 
+
+/* sample user implementation of GC_objdup */
+void * GC_objdup(void *p)
+{
+  size_t sz;
+  GC_alloc_func alloc = GC_alloc_fn_and_size(p, &sz);
+  void *newObj = alloc(sz);
+  memcpy(newObj, p, sz);
+  return newObj;
+}
+
+
+void test_objdup(char *p, int sz)
+{
+  char *q;
+  int i;
+  for (i=0; i<sz; ++i) p[i] = i; /* will truncate */
+  q = (char*)GC_objdup(p);
+  if (memcmp(p,q,sz) != 0){
+    fprintf(stderr, "objdup result wrong for size %d\n", sz);
+    failed = 1;
+  }
+}
+
+
 int main(void) {
   int i;
 
@@ -40,6 +66,8 @@ int main(void) {
 #   ifdef ATOMIC_UNCOLLECTABLE
     test(p4, i, "GC_malloc_atomic_uncollectable", GC_malloc_atomic_uncollectable);
 #   endif
+
+    test_objdup(p2, i);
   }
 
   return failed;
